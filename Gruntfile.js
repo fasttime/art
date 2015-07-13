a499ecea599ae556b7e3f3d7be833538d6cc65a8
Francesco Trotta <>
2015-07-14 00:21:14 +0200

    New repo

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+/* jshint node: true */
+
+'use strict';
+
+module.exports =
+    function (grunt)
+    {
+        // Project configuration.
+        grunt.initConfig(
+            {
+                clean: { default: ['coverage', 'lib/**/*.min.js', 'art.md'] },
+                jscs:
+                {
+                    default: ['*.js', 'lib/**/*.js'],
+                    options:
+                    {
+                        // Encourage use of abbreviations: "char", "obj", "str".
+                        disallowIdentifierNames: ['character', 'object', 'string'],
+                        disallowMixedSpacesAndTabs: true,
+                        disallowSpaceAfterObjectKeys: true,
+                        disallowSpaceAfterPrefixUnaryOperators: true,
+                        disallowSpaceBeforePostfixUnaryOperators: true,
+                        disallowSpacesInCallExpression: true,
+                        disallowSpacesInFunctionDeclaration: { beforeOpeningRoundBrace: true },
+                        disallowSpacesInNamedFunctionExpression: { beforeOpeningRoundBrace: true },
+                        disallowSpacesInsideBrackets: true,
+                        disallowSpacesInsideParentheses: true,
+                        disallowTrailingWhitespace: 'ignoreEmptyLines',
+                        disallowYodaConditions: true,
+                        requireBlocksOnNewline: 1,
+                        requireKeywordsOnNewLine:
+                        [
+                            'break',
+                            'case',
+                            'catch',
+                            'continue',
+                            'default',
+                            'do',
+                            'else',
+                            'finally',
+                            'for',
+                            'switch',
+                            'throw',
+                            'try'
+                        ],
+                        requireLineBreakAfterVariableAssignment: true,
+                        requireLineFeedAtFileEnd: true,
+                        requirePaddingNewLinesAfterUseStrict: true,
+                        requireSpaceAfterBinaryOperators: true,
+                        requireSpaceAfterKeywords: true,
+                        requireSpaceAfterLineComment: true,
+                        requireSpaceBeforeBinaryOperators: true,
+                        requireSpaceBeforeBlockStatements: true,
+                        requireSpaceBeforeKeywords:
+                        [
+                            'delete',
+                            'if',
+                            'in',
+                            'instanceof',
+                            'return',
+                            'while'
+                        ],
+                        requireSpaceBeforeObjectValues: true,
+                        requireSpaceBetweenArguments: true,
+                        requireSpacesInAnonymousFunctionExpression:
+                        {
+                            beforeOpeningRoundBrace: true
+                        },
+                        requireSpacesInConditionalExpression: true,
+                        requireSpacesInForStatement: true,
+                        requireSpacesInFunctionDeclaration: { beforeOpeningCurlyBrace: true },
+                        requireSpacesInFunctionExpression: { beforeOpeningCurlyBrace: true },
+                        requireSpacesInsideObjectBrackets: 'all',
+                        validateAlignedFunctionParameters: true,
+                        validateIndentation: 4,
+                        validateParameterSeparator: ', '
+                    }
+                },
+                jsdoc2md: { default: { dest: 'art.md', src: 'lib/art.js' } },
+                jshint:
+                {
+                    default: ['*.js', 'lib/**/*.js'],
+                    options:
+                    {
+                        curly: true,
+                        eqeqeq: true,
+                        immed: true,
+                        latedef: 'nofunc',
+                        maxlen: 100,
+                        newcap: false,
+                        noarg: true,
+                        noempty: true,
+                        quotmark: true,
+                        singleGroups: true,
+                        strict: true,
+                        trailing: true,
+                        undef: true,
+                        unused: true,
+                        
+                        boss: true,
+                        elision: true,
+                        eqnull: true,
+                        evil: true,
+                        validthis: true,
+                        '-W018': true,
+                    }
+                },
+                uglify:
+                {
+                    default: { files: { 'lib/art.min.js': 'lib/art.js' } },
+                    options: { compress: { global_defs: { DEBUG: false }, hoist_vars: true } }
+                }
+            }
+        );
+        
+        // These plugins provide necessary tasks.
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-jscs');
+        
+        // Default task.
+        grunt.registerTask('default', ['clean', 'jshint', 'jscs', 'uglify']);
+    };
---
+++ README.md
@@ -0,0 +1,53 @@
+art
+=======
+
+A really small DOM manipulation library
+
+The **art** library makes it easy to dynamically create and manipulate DOM elements.
+
+Create an empty `DIV`:
+
+```js
+var myDiv = art("div");
+```
+
+Append the text "Hello, World!" to the document body:
+
+```js
+art(document.body, "Hello World!");
+```
+
+Create a button with a caption and a click handler:
+
+```js
+var button =
+    art(
+        "input",
+        { type: "button", value: "Click me" },
+        art.on("click", function () { alert("Bravo!"); })
+    );
+```
+
+Create a 2x2 table with centered text and append it to an existing element:
+
+```js
+art(
+    document.getElementById("myParent"),
+    art(
+        "table",
+        { style: { textAlign: "center" } },
+        art(
+            "tr",
+            art("td", "top left"),
+            art("td", "top right")
+        ),
+        art(
+            "tr",
+            art("td", "bottom left"),
+            art("td", "bottom right")
+        )
+    )
+);
+```
+
+*This is a preliminary documentation and may be subject to change at any time.*
